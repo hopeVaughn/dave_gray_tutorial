4dbbd32a8701518621a271c5076c6c78cf55f3bd
Hope <hopevaughnwarren@gmail.com>
2023-09-24 09:22:00 -0700

    finish useContext lesson

--- 03_reducer/src/App.tsx
+++ 03_reducer/src/App.tsx
@@ -1,13 +1,15 @@
 import Counter from "./Counter";
-
-
+import { CounterProvider } from "./context/CounterContext";
+import { initialState } from "./context/CounterContext";
 
 
 
 function App() {
   return (
     <>
-      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      <CounterProvider count={initialState.count} text={initialState.text} >
+        <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      </CounterProvider>
     </>
   )
 }
